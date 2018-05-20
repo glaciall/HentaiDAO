d5c4aa4a6aac957d96dc15584ad3eb368e39c496
matrixy <matrixy@me.com>
2018-05-20 22:28:17 +0800

    改造以适应参数化查询

--- QuickDAO/src/main/java/cn/org/hentai/dao/JDBCBridge.java
+++ QuickDAO/src/main/java/cn/org/hentai/dao/JDBCBridge.java
@@ -4,59 +4,58 @@ import java.util.List;
 
 /**
  * Created by matrixy on 2017/11/10.
+ * JDBC执行适配器
  */
 public interface JDBCBridge
 {
     /**
-     * 执行insert语句
-     * @param sql
-     * @param values
-     * @return
+     * 执行INSERT插入语句
+     * @param sql 待执行的INSERT语句
+     * @param values INSERT语句的参数数组
+     * @return 新记录的主键值
      */
     public <E> E insert(String sql, Object... values);
 
     /**
-     * 执行DML操作
-     * @param sql
-     * @param values
-     * @return
+     * 执行DML语句
+     * @param sql 待执行的SQL语句
+     * @param values DML语句的参数数组
+     * @return 影响的行数
      */
     public <E> E execute(String sql, Object... values);
 
     /**
-     * 执行update修改
-     * @param sql
-     * @param values
-     * @return
+     * 执行UPDATE语句
+     * @param sql 待执行的UPDATE语句
+     * @param values UPDATE语句的参数数组
+     * @return 影响的行数
      */
     public <E> E update(String sql, Object... values);
 
     /**
-     * 查询一行结果
-     * @param sql
-     * @param type
-     * @param <E>
-     * @return
+     * 查询以得到单个结果
+     * @param sql 待执行的SELECT语句
+     * @param type 查询结果到实体类的映射类，期待返回此类的实例
+     * @param values SELECT语句的参数数组
+     * @return 查询结果到type类实例化的实体
      */
-    public <E> E queryOne(String sql, Class type);
+    public <E> E queryOne(String sql, Class type, Object... values);
 
     /**
-     * 查询一个列表
-     * @param sql
-     * @param type
-     * @param values
-     * @param <E>
-     * @return
+     * 查询列表
+     * @param sql 待执行的SELECT语句
+     * @param type 查询结果到实体类的映射类，期待返回此类的实例列表
+     * @param values SELECT语句的参数数组
+     * @return 查询结果列表
      */
     public <E> List<E> query(String sql, Class type, Object... values);
 
     /**
-     * 查询并返回一个值
-     * @param sql
-     * @param type
-     * @param values
-     * @param <E>
-     * @return
+     * 查询值
+     * @param sql 待执行的SELECT语句
+     * @param type 值的类型类，期待返回此类的结果值
+     * @param values SELECT语句的参数数组
+     * @return 由type指定值
      */
     public <E> E queryForValue(String sql, Class type, Object... values);
 }
